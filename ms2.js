661739538527d14bb9f0f5b0ad0ec6738fa7ee86
Fares-Basousy <faresbasousy@gmail.com>
2023-05-15 16:15:39 +0300

    Microservice 2 (Storage)

--- ms2.js
+++ ms2.js
@@ -4,19 +4,19 @@ quantity = {
     item2 : 276,
     item3 : 770
 };
-// Require Express to run server and routes
+//Require Express to run server and routes
 const express = require("express");
-// Start up an instance of app2
+//Start up an instance of app2
 const app2 = express() ;
-/* Middleware*/
+//Middleware
 //Here we are configuring express to use body-parser as middle-ware.
 const bodyParser = require("body-parser");
 app2.set(bodyParser.urlencoded({ extended: false }));
 app2.use(bodyParser.json());
-// Cors for cross origin allowance
+//Cors for cross origin allowance
 const Cors = require("cors")
 app2.use(Cors());
-// Setup Server
+//Setup Server
 
 const port = 10000;
 const server = app2.listen(port,()=>{console.log(`running on http://localhost:${port}/`)})
